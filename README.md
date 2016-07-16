9d7f0d353db8840c3d7fbcfb49ea2a0bdd136b64
Darksmurf <etleyden@gmail.com>
2016-07-16 10:34:44 -0500

    Cleaned up CSS, Added to README.md, added fonts, and icons

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # history.awesomeify
-Chrome Extension to awesomify your history. This extension will provide analytics, and many other capabilities to your Chrome history (chrome://history).
+Chrome Extension to awesomify your history. This extension 
+will provide analytics, and many other capabilities to your 
+Chrome history (chrome://history).
+
+This extension is based on the concept represented in 
+[this](https://medium.freecodecamp.com/browserhistory-2abad38022b1#.wt2tq8g1k) 
+article. The concept images in the article can be found 
+in the "concepts" folder in the repo for quick access.
--- script.js
+++ script.js
@@ -1,32 +1,36 @@
 //Functions that link to other `chrome://` urls
 function navToHistory() {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.update(tabs[0].id, {url: "chrome://history"});
-  });
+        chrome.tabs.update(tabs[0].id, {url: "chrome://history"});
+    });
 
 }
 function navToExtensions() {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.update(tabs[0].id, {url: "chrome://extensions"});
-  });
+        chrome.tabs.update(tabs[0].id, {url: "chrome://extensions"});
+    });
 
 }
 function navToSettings() {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.update(tabs[0].id, {url: "chrome://settings"});
-  });
+        chrome.tabs.update(tabs[0].id, {url: "chrome://settings"});
+    });
 
 }
 function navToAbout() {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.update(tabs[0].id, {url: "chrome://version"});
-  });
-
+        chrome.tabs.update(tabs[0].id, {url: "chrome://version"});
+    });
+}
+function navToCredits() {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.update(tabs[0].id, {url: "credits.html"});
+    });
 }
 $(document).ready(function() {
     $('#extensions').click(navToExtensions);
     $('#settings').click(navToSettings);
-    $('#bookmarks').click(navToBookmarks);
     $('#history').click(navToHistory);
     $('#about').click(navToAbout);
+    $('#credits').click(navToCredits);
 });
