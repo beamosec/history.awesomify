e882bed96aa9f4291a310fde304da5ccf8ebdf71
Ethan Leyden <etleyden@gmail.com>
2016-07-16 09:05:26 -0500

    Revert "links open in tabs"

--- script.js
+++ script.js
@@ -1,18 +1,8 @@
 //Functions that link to other `chrome://` urls
 function navToExtensions() {
-    console.log('extensions!');
-    chrome.tabs.create({url:"chrome://extensions"});
-}
-function navToSettings() {
-  console.log('settings!');
-  chrome.tabs.create({url:"chrome://settings"});
-}
-function navToBookmarks() {
-  console.log('bookmarks!');
-  chrome.tabs.create({url:"chrome://bookmarks"});
+    console.log('running');
+    window.location = "chrome://extensions";
 }
 $(document).ready(function() {
     $('#extensions').click(navToExtensions);
-    $('#settings').click(navToSettings);
-    $('#bookmarks').click(navToBookmarks);
 });
