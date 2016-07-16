67a359c40f27e8ad9424493ab17661dd33fc5fe8
Paul Hosler <pahosler@gmail.com>
2016-07-16 07:14:39 -0500

    #opens in tabs

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 //Functions that link to other `chrome://` urls
 function navToExtensions() {
     console.log('extensions!');
-    chrome.tabs.create(windowId,{url:"chrome://extensions"});
+    chrome.tabs.create({url:"chrome://extensions"});
 }
 function navToSettings() {
   console.log('settings!');
-  chrome.tabs.create(windowId,{url:"chrome://settings"});
+  chrome.tabs.create({url:"chrome://settings"});
 }
 function navToBookmarks() {
   console.log('bookmarks!');
-  chrome.tabs.create(windowId,{url:"chrome://bookmarks"});
+  chrome.tabs.create({url:"chrome://bookmarks"});
 }
 $(document).ready(function() {
     $('#extensions').click(navToExtensions);
