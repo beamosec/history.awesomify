fdd15054cce8e584b81f2f64b8cd22febbebc799
Darksmurf <etleyden@gmail.com>
2016-08-07 17:49:26 -0400

    Linked slider to timeline scrolling

--- scripts/main.js
+++ scripts/main.js
@@ -1,26 +1,53 @@
 var open = true; //to manage the status of the .top-nav
+var domains = {};//variable to hold domain objects
+var sliderDistPercentage;
 
 $(document).ready(function() {
 	
     //Manages top menu bar
     checkWinSize();
 	
-    $('.slider').draggable({
+	var startDistance, stopDistance;
+	sliderDistPercentage = getSliderDistPercent();
+	
+	$('.slider').draggable({
         axis: 'x',
         scroll: "false",
-        containment: ".slider-track",
+        containment: ".slider-container",
         cursor: 'grab',
+		start: function(event, ui) {
+			startDistance = ui.position.left;
+		},
 		drag: function(event, ui) {
-			var draggableWidth = ui.helper.width();
-			var timelineWidth = $('.visited').width();
-			console.log(timelineWidth);
+			sliderDistPercentage = roundNearestPercent(getSliderDistPercent());
+			
+			var timelineWidth = getMaxScrollVal($('.timeline'));
+			
+			$('.timeline').scrollLeft(timelineWidth * sliderDistPercentage);
+			
 		}
-    })
+    });
     
 	$('.expand').click(toggleMenu);
+	
     $(window).on('resize', checkWinSize);
+	
+	/* 
+	 * this code is exclusive to resize
+	 * checkWinSize is called onload, thus making 
+	 * this a better way to handle redrawing 
+	 * the slider component
+	 */
+	$(window).on('resize', function() {
+		//also represents width of the area for the slider
+		var farthestPos = $('.slider-container').width() - $('.slider').width();
+		var newSliderPos = farthestPos * sliderDistPercentage;
 		
-	var startTime = (new Date).getTime();
+		$('.slider').css({
+			'left': newSliderPos
+		});
+		
+	});
 	
 	//maxResults must be Math.pow(2, 31), because the requirement is
 	//a 32 bit unsigned integer.
@@ -34,14 +61,12 @@ $(document).ready(function() {
 		maxResults: getLargestIntOfSize(31),
 	}, function(results) {
 		
-		//variable to hold domain objects
-		var domains = {};
-		
 		//grouping domain names
 		for(let i in results) {
 		
 			var domainKeys = Object.keys(domains);
 			var currDomain = extractSiteName(results[i].url);
+			var currDomainIndex = currDomain;
 			var currDomain_plain = currDomain.split('.').join('');
 			var currDomainIndexExists = domainKeys.indexOf(currDomain_plain) > -1;
 			
@@ -52,6 +77,7 @@ $(document).ready(function() {
 					if(domainKeys[j].indexOf(currDomain_plain) > -1) {
 						
 						currDomainIndexExists = true;
+						currDomainIndex = domainKeys[j];
 						
 						break;
 					
@@ -59,32 +85,51 @@ $(document).ready(function() {
 					}
 				
 				}
+				
 			}
 			if(!currDomainIndexExists) {
 			
-				domains[currDomain_plain] = {
-					'url': results[i].url,
+				domains[currDomainIndex] = {
+					'urls': [results[i].url],
 					'domain': currDomain,
 				}
 			
+			} else {
+				
+				domains[currDomainIndex].urls.push(results[i].url);
+				
 			}
 			
 		} //end of domain grouping
 		
 		var domainKeys = Object.keys(domains);
 		
-		//get favicon urls. Seperate for understandability. Keep it that way.
+		//get favicon urls. Seperate from domain grouping
+		//for understandability. Keep it that way.
 		for(let i in domainKeys) {
+			
 			domains[domainKeys[i]].favicon = "chrome://favicon/" +
-				domains[domainKeys[i]].url;
-			document.write('<img height="20" src="' + domains[domainKeys[i]].favicon + '"/>');	
+				domains[domainKeys[i]].urls[0];
+			
 		}
-		
-		
 		
 	});
 	
-});
+}); //end of $(document).ready();
+
+function getMaxScrollVal(element) {
+	var currScroll = element.scrollLeft();
+	element.scrollLeft(getLargestIntOfSize(31));
+	var maxScrollVal = element.scrollLeft();
+	element.scrollLeft(currScroll);
+	return maxScrollVal;
+}
+function getSliderDistPercent() {
+	var sliderPos = getPosAsInt($('.slider').css('left'), 'px');
+	var farthestPos = $('.slider-container').width() - $('.slider').width();
+	
+	return sliderPos / farthestPos;
+}
 
 function extractSiteName(url) {
 	
@@ -121,8 +166,17 @@ function extractDomain(url) {
 
 }
 
+//get date object from milliseconds since epoch
+function getDate(milliseconds) {
+	
+	return new Date(milliseconds);
+
+}
+
 //.top-nav display functions
 function toggleMenu() {
+	
+	console.log("toggleMenu");
 	
     $('.collapsible').css('display', open ? 'block' : 'none');
     $('.collapsible > *').css('display', open ? 'block': 'none');
@@ -132,17 +186,31 @@ function toggleMenu() {
 }
 
 //check if window is large enough to fit certain UI elements.
-function checkWinSize() {
-
-	var bool = window.innerWidth < 1100;
+function checkWinSize() { //also properly redraws the slider.
+	//for some reason, the slider doesn't retain good form
+	// (but it does when you refresh, given that this code doesn't exist)
+	
+	console.log('checkWinSize');
+	setTimeout(function() {
+		console.clear();
+	}, 1);
+	
+	var bool = window.innerWidth <= 1100;
     
 	$('.expand').css('display', bool ? 'inline' : 'none');
     $('#menu-icon').css('display', bool ? 'inline' : 'none');
     $('.collapsible').css('display', bool ? 'none' : 'inline');
     $('.collapsible > *').css('display', bool ? 'none' : 'inline');
-
 }
 
+function roundNearestPercent(value) {
+	return Math.round(value * 100) / 100;
+}
+
+//removes "remove" from "str" and returns integer value
+function getPosAsInt(str) {
+	return parseInt(str.substr(0, str.length - 2));
+}
 //function to get largest unsigned integer of a certain length
 //Takes 1 argument (number of bits)
 function getLargestIntOfSize(bits) {
